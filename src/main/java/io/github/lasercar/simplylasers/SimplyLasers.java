77c4bcd329ad3c21d0c1f630d36179e2043aad69
Lasercar7 <lasercar@users.noreply.github.com>
2015-05-13 16:42:18 -0500

    base mod class

---
+++ src/main/java/io/github/lasercar/simplylasers/SimplyLasers.java
@@ -0,0 +1,9 @@
+package io.github.lasercar.simplylasers;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid="SimplyLasers", name="SimplyLasers", version="1.7.10-1.0")
+
+public class SimplyLasers {
+
+}
