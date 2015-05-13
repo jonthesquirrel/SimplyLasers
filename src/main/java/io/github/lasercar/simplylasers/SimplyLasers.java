1a8a4b0781f248d04c0e1ee105c13991f26d2c10
Lasercar7 <lasercar@users.noreply.github.com>
2015-05-13 16:55:55 -0500

    mod class - hook into FML events

--- src/main/java/io/github/lasercar/simplylasers/SimplyLasers.java
+++ src/main/java/io/github/lasercar/simplylasers/SimplyLasers.java
@@ -1,9 +1,28 @@
 package io.github.lasercar.simplylasers;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="SimplyLasers", name="SimplyLasers", version="1.7.10-1.0")
-
 public class SimplyLasers {
 
+    @Mod.Instance("SimplyLasers")
+    public static SimplyLasers instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
 }
